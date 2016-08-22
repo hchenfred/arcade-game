8201187f1bd1cb7cd5ee53e3f0d8934b47b27db8
Huan Chen <hchen.fred@gmail.com>
2016-08-21 21:42:22 -0700

    Added arcade project

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +16,15 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+    if (this.x > 505) {
+        this.speed = this.randomSpeed();
+        this.x = 0;
+    }
+};
+
+Enemy.prototype.randomSpeed = function () {
+    return Math.random() * 1000;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,14 +32,40 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
+}
+
+Player.prototype.update = function () {
+
+};
+
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyPressed) {
+    if (keyPressed === 'left') {
+
+    }
+
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var enemy1 = new Enemy(0, 60, Math.random() * 1000);
+var enemy2 = new Enemy(0, 140, Math.random() * 1000);
+var enemy3 = new Enemy(0, 220, Math.random() * 1000);
+var player = new Player(303, 400);
+var allEnemies = [enemy1, enemy2, enemy3];
 
 
 
