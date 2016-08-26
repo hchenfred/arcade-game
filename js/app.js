6b118fd188c4b46c62eecdfc306df34edb7f2c14
Huan Chen <hchen.fred@gmail.com>
2016-08-26 07:01:09 -0700

    Enabled basic functionality

--- js/app.js
+++ js/app.js
@@ -1,21 +1,14 @@
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
     this.x = x;
     this.y = y;
     this.speed = speed;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     this.x += this.speed * dt;
     if (this.x > 505) {
         this.speed = this.randomSpeed();
@@ -23,54 +16,74 @@ Enemy.prototype.update = function(dt) {
     }
 };
 
+// Generate a random speed for enemy bugs
 Enemy.prototype.randomSpeed = function () {
-    return Math.random() * 1000;
+    return Math.random() * 300;
 };
 
-// Draw the enemy on the screen, required method for game
+// Render the enemy bugs on the web page
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player class
 var Player = function(x, y) {
     this.x = x;
     this.y = y;
     this.sprite = 'images/char-boy.png';
+    this.speed = 5;
 }
 
+// Reset the player when a collision is detected
 Player.prototype.update = function () {
-
+    for (var i = 0; i < allEnemies.length; i++) {
+        if (this.x < allEnemies[i].x + 101 && this.x + 101 > allEnemies[i].x && this.y < allEnemies[i].y + 65 && 65 + this.y > allEnemies[i].y) {
+            console.log("player " + this.x + " " + this.y);
+            console.log("enemy " + allEnemies[i].x + " " + allEnemies[i].y);
+            this.x = 202;
+            this.y = 408;
+        }
+    }
 };
 
+// Render the player on the screen
 Player.prototype.render = function () {
+    //console.log(this.x + " " + this.y);
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function(keyPressed) {
-    if (keyPressed === 'left') {
+// Handler for moving the player
+Player.prototype.handleInput = function(keyPressed, dt) {
+    if (keyPressed === 'left' && this.x > 0) {
+        this.x -= 101;
 
+    } else if (keyPressed === 'right' && this.x < 505) {
+        this.x += 101;
+    } else if (keyPressed === 'up' && this.y > 0) {
+        this.y -= 83;
+        console.log("y value is " + this.y);
+    } else if (keyPressed === 'down' && this.y < 606) {
+        this.y += 83;
+    } else {
+        console.log("reset");
+        this.x = 202;
+        this.y = 408;
     }
-
 };
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-var enemy1 = new Enemy(0, 60, Math.random() * 1000);
-var enemy2 = new Enemy(0, 140, Math.random() * 1000);
-var enemy3 = new Enemy(0, 220, Math.random() * 1000);
-var player = new Player(303, 400);
+var enemy1 = new Enemy(0, 65, Math.random() * 1000);
+var enemy2 = new Enemy(0, 145, Math.random() * 1000);
+var enemy3 = new Enemy(0, 225, Math.random() * 1000);
+var player = new Player(202, 408);
 var allEnemies = [enemy1, enemy2, enemy3];
 
 
+$('.startBtn').click(function(e) {
+  alert( "hello" );
+  console.log("hello");
+});
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -78,6 +91,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
